0ca90309d26d63075d01f47d5d5d476e9e6cee9f
Hussain <mhussainprog@gmail.com>
2026-03-26 20:38:26 +0500

    remove files

--- app/config/config.py
+++ app/config/config.py
@@ -1,16 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Auth Boilerplate"
-    SECRET_KEY: str = "your-super-secret-key"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
+    DATABASE_URL: str
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
     GOOGLE_REDIRECT_URI: str = "http://localhost:8000/auth/google/callback"
 
-    class Config:
-        env_file = ".env"
+    # This line tells Pydantic to read from the .env file
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
